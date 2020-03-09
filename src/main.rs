409d7682dd4531163dada36c85006f763a258627
Christopher Esterhuyse <christopher.esterhuyse@gmail.com>
2020-03-09 10:47:06 +0100

    nice

--- src/main.rs
+++ src/main.rs
@@ -90,31 +90,41 @@ impl Arrow {
     }
 }
 // invariant: self.0.next_index - 1 is within bounds of self.0.v
-struct Entry<'vec, 'entry, T>(&'entry mut Draining<'vec, T>);
+struct Entry<'vec, 'entry, T> {
+    draining: &'entry mut Draining<'vec, T>,
+}
 
 impl<'vec, 'entry, T> Entry<'vec, 'entry, T> {
     fn take(self) -> T {
-        let d = self.0;
-        d.next_index -= 1;
-        d.v.remove(d.next_index)
+        self.draining.next_index -= 1; // breaks invariant. no longer off-by-one
+        self.draining.vec.remove(self.draining.next_index)
     }
     fn get_mut(&mut self) -> &mut T {
-        unsafe { self.0.v.get_unchecked_mut(self.0.next_index - 1) }
+        // invariant: off by one
+        unsafe { self.draining.vec.get_unchecked_mut(self.draining.next_index - 1) }
     }
 }
+
+// two implicit states:
+// 1. not borrowed by Entry: self.next index is the index of the next element
+// 2. borrowed by Entry: self.next_index is 1 greater than the index of next element
+//
+// conceptually, could be alternatively achieved by either:
+// 1. always being in state 2, where Entry gets the old copy of next_index (so its no longer off-by-one for Entry)
+// 2. incrementing next_index whenever Entry is DROPPED. (drops are not guaranteed => unsafe!)
 struct Draining<'vec, T> {
     next_index: usize, // always off by one while Draining is borrowed by an Entry
-    v: &'vec mut Vec<T>,
+    vec: &'vec mut Vec<T>,
 }
 impl<'vec, T> Draining<'vec, T> {
-    fn new(v: &'vec mut Vec<T>) -> Self {
-        Self { v, next_index: 0 }
+    fn new(vec: &'vec mut Vec<T>) -> Self {
+        Self { vec, next_index: 0 }
     }
     fn next<'entry>(&'entry mut self) -> Option<Entry<'vec, 'entry, T>> {
         // checks invariant
-        if self.next_index < self.v.len() {
+        if self.next_index < self.vec.len() {
             self.next_index += 1;
-            Some(Entry(self))
+            Some(Entry { draining: self })
         } else {
             None
         }
