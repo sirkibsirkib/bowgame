453522cc0f5c5ae69164f8a9d18cd8629cd610ec
Christopher Esterhuyse <christopher.esterhuyse@gmail.com>
2020-03-11 08:52:30 +0100

    spritebatch

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
+use ggez::audio::SoundSource;
+use ggez::audio::Source;
 use ggez::event::EventHandler;
 use ggez::event::MouseButton;
 use ggez::{
-    graphics::{self, Color, DrawMode, DrawParam, Mesh, MeshBuilder, Rect, BLACK, WHITE},
-    nalgebra as na, *,
+    graphics::{
+        self, spritebatch::SpriteBatch, Color, DrawMode, DrawParam, Image, Mesh, MeshBuilder, Rect,
+        BLACK, WHITE,
+    },
+    nalgebra::{self as na, geometry::Rotation2},
+    *,
 };
 
 const BROWN: Color = Color { r: 0.5, g: 0.2, b: 0.2, a: 1. };
@@ -18,7 +24,14 @@ fn main() {
     let (mut ctx, mut event_loop) = ContextBuilder::new("my_game", "Cool Game Author")
         .build()
         .expect("aieee, could not create ggez context!");
+    let arrow_png = Image::new(&mut ctx, "/arrow.png").unwrap();
     let mut my_game = MyGame {
+        dude: [100., 90.].into(),
+        arrow_batch: SpriteBatch::new(arrow_png),
+        twang1: ggez::audio::Source::new(&mut ctx, "/twang1.wav").unwrap(),
+        twang2: ggez::audio::Source::new(&mut ctx, "/twang2.wav").unwrap(),
+        twang3: ggez::audio::Source::new(&mut ctx, "/twang3.wav").unwrap(),
+        time: 0.,
         arrows: vec![],
         stuck_arrows: vec![],
         mouse_click_at: None,
@@ -27,6 +40,11 @@ fn main() {
                 &[[0., -HEAD_THICKNESS], [HEAD_LENGTH, 0.], [0., HEAD_THICKNESS]],
                 graphics::WHITE,
             )
+            .unwrap()
+            .build(&mut ctx)
+            .unwrap(),
+        unit_line: MeshBuilder::new()
+            .line(&[[0., 0.], [1., 0.]], 1., graphics::WHITE)
             .unwrap()
             .build(&mut ctx)
             .unwrap(),
@@ -58,35 +76,40 @@ fn main() {
         Err(e) => println!("Error occured: {}", e),
     }
 }
+type Pt2 = na::Point2<f32>;
 
 struct MyGame {
+    arrow_batch: SpriteBatch,
+    dude: Pt2,
+    twang1: Source,
+    twang2: Source,
+    twang3: Source,
+    time: f32,
     arrowhead: Mesh,
     arrowshaft: Mesh,
+    unit_line: Mesh,
     arrows: Vec<Arrow>,
     stuck_arrows: Vec<StuckArrow>,
-    mouse_click_at: Option<na::Point2<f32>>,
+    mouse_click_at: Option<Pt2>,
 }
 struct Arrow {
-    pos: na::Point2<f32>,
-    vel: na::Point2<f32>,
+    pos: Pt2,
+    vel: Pt2,
     rot: f32,
     age: u8,
 }
 struct StuckArrow {
     pos: na::Point2<f32>,
     rot: f32,
+    vibration_amplitude: f32,
 }
 impl Arrow {
-    fn vel_to_rot(vel: na::Point2<f32>) -> f32 {
-        nalgebra::geometry::Rotation2::rotation_between(
-            &na::Point2::new(1., 0.).coords,
-            &vel.coords,
-        )
-        .angle()
+    fn vel_to_rot(vel: Pt2) -> f32 {
+        Rotation2::rotation_between(&Pt2::new(1., 0.).coords, &vel.coords).angle()
     }
     fn stick(self) -> StuckArrow {
         let Self { pos, rot, .. } = self;
-        StuckArrow { pos, rot }
+        StuckArrow { pos, rot, vibration_amplitude: 0.5 }
     }
 }
 // invariant: self.0.next_index - 1 is within bounds of self.0.v
@@ -133,17 +156,26 @@ impl<'vec, T> Draining<'vec, T> {
 
 impl EventHandler for MyGame {
     fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+        self.time += 1.0;
         let mut draining = Draining::new(&mut self.arrows);
         while let Some(mut entry) = draining.next() {
             let arrow: &mut Arrow = entry.get_mut();
             arrow.pos += arrow.vel.coords;
-            arrow.vel = nalgebra::geometry::Rotation2::new(0.01).transform_point(&arrow.vel);
-            arrow.rot = Arrow::vel_to_rot(arrow.vel);
             if arrow.age > 17 {
+                match unsafe { std::mem::transmute::<_, f32>(self.time) } as usize % 3 {
+                    0 => &mut self.twang1,
+                    1 => &mut self.twang2,
+                    _ => &mut self.twang3,
+                }
+                .play()
+                .unwrap();
                 self.stuck_arrows.push(entry.take().stick())
             } else {
                 arrow.age += 1;
             }
+        }
+        for stuck_arrow in &mut self.stuck_arrows {
+            stuck_arrow.vibration_amplitude *= 0.93
         }
         Ok(())
     }
@@ -157,9 +189,14 @@ impl EventHandler for MyGame {
     fn mouse_button_up_event(&mut self, _ctx: &mut Context, button: MouseButton, x: f32, y: f32) {
         if let MouseButton::Left = button {
             if let Some(first) = self.mouse_click_at.take() {
-                let second: na::Point2<f32> = [x, y].into();
-                let vel = (second - first.coords) * 0.3;
-                self.arrows.push(Arrow { age: 0, pos: second, vel, rot: Arrow::vel_to_rot(vel) });
+                let second: Pt2 = [x, y].into();
+                let vel = (first - second.coords) * 0.214;
+                self.arrows.push(Arrow {
+                    age: 0,
+                    pos: self.dude,
+                    vel,
+                    rot: Arrow::vel_to_rot(vel),
+                });
             }
         } else {
             self.stuck_arrows.extend(self.arrows.drain(..).map(Arrow::stick));
@@ -171,40 +208,61 @@ impl EventHandler for MyGame {
         for arrow in &self.arrows {
             let dest = arrow.pos.into();
             let rotation = arrow.rot;
-            graphics::draw(
-                ctx,
-                &self.arrowhead,
-                DrawParam {
-                    dest,
-                    color: WHITE,
-                    scale: [20.; 2].into(),
-                    rotation,
-                    ..Default::default()
-                },
-            )?;
-            graphics::draw(
-                ctx,
-                &self.arrowshaft,
-                DrawParam {
-                    dest,
-                    color: WHITE,
-                    scale: [20.; 2].into(),
-                    rotation,
-                    ..Default::default()
-                },
-            )?;
+            let p = DrawParam {
+                src: Rect { x: 0., y: 0., h: 0.1, w: 1. },
+                dest,
+                color: WHITE,
+                scale: [40. / 230.; 2].into(),
+                rotation,
+                offset: [0.95, 0.5].into(),
+                ..Default::default()
+            };
+            self.arrow_batch.add(p);
+            // graphics::draw(ctx, &self.arrowhead, p)?;
         }
         for stuck_arrow in &mut self.stuck_arrows {
             let dest = stuck_arrow.pos.into();
-            let rotation = stuck_arrow.rot;
+            let rotation =
+                stuck_arrow.rot + (self.time * 3.0).sin() * stuck_arrow.vibration_amplitude;
+            let p = DrawParam {
+                src: Rect { x: 0., y: 0., h: 0.1, w: 1. },
+                dest,
+                color: WHITE,
+                scale: [40. / 230.; 2].into(),
+                rotation,
+                offset: [0.95, 0.5].into(),
+                ..Default::default()
+            };
+            // graphics::draw(ctx, &self.arrowshaft, p)?;
+
+            self.arrow_batch.add(p);
+        }
+        graphics::draw(ctx, &self.arrow_batch, DrawParam::default())?;
+        self.arrow_batch.clear();
+        if let Some(start) = self.mouse_click_at {
+            let end: Pt2 = ggez::input::mouse::position(ctx).into();
+            let dif = end - start.coords;
+            let rotation =
+                Rotation2::rotation_between(&Pt2::new(1., 0.).coords, &dif.coords).angle();
             graphics::draw(
                 ctx,
-                &self.arrowshaft,
+                &self.unit_line,
                 DrawParam {
-                    dest,
+                    dest: start.into(),
                     color: WHITE,
-                    scale: [20.; 2].into(),
+                    scale: [dif.coords.norm(), 1.0].into(),
                     rotation,
+                    ..Default::default()
+                },
+            )?;
+            graphics::draw(
+                ctx,
+                &self.unit_line,
+                DrawParam {
+                    dest: self.dude.into(),
+                    color: RED,
+                    scale: [dif.coords.norm() * 4.0, 1.0].into(),
+                    rotation: rotation + core::f32::consts::PI,
                     ..Default::default()
                 },
             )?;
